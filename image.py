35e244b522de0285a865081c0fa81368c7ef5f1e
Bhuvanesh-Suresh <bhuvanesh1112006@gmail.com>
2023-10-05 09:29:35 +0530

    Bhuvanesh

--- image.py
+++ image.py
@@ -1,32 +1,3 @@
-# Developing a Simple Webserver
-## Name:Bhuvanesh.S.R
-## Ref no:23013380
-# AIM:
-
-Develop a webserver to display about top five web application development frameworks.
-
-# DESIGN STEPS:
-
-## Step 1:
-
-HTML content creation is done
-
-## Step 2:
-
-Design of webserver workflow
-
-## Step 3:
-
-Implementation using Python code
-
-## Step 4:
-
-Serving the HTML pages.
-
-## Step 5:
-
-Testing the webserver
-# PROGRAM:
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 content = """
@@ -50,8 +21,4 @@ class HelloHandler(BaseHTTPRequestHandler):
 server_address = ('', 80)
 httpd = HTTPServer (server_address, HelloHandler)
 httpd.serve_forever()
-# OUTPUT:
 
-# RESULT:
-
-The program is executed succesfully
